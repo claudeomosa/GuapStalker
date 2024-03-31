f778a4faa792e519e18a68badfb5c37df0fe7d4d
claudeomosa <claudeomosa@gmail.com>
2024-03-31 08:23:09 +0300

    Feat: add a simple GraphQL Query

---
+++ api/GuapStalker.api/Queries/UserQuery.cs
@@ -0,0 +1,13 @@
+﻿using database.Models;
+
+namespace api.Queries
+{
+    public class UserQuery
+    {
+        public User GetUser() =>
+            new User
+            {
+                Username = "test",
+            };
+    }
+}
