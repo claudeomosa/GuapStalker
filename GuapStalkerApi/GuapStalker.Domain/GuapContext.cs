23b06dcc53ae9bcb9ba8bac32dfe945363309774
claudeomosa <claudeomosa@gmail.com>
2024-08-04 11:54:46 +0300

    feat: add the Domain project -- responsible for DB connection and service classes

---
+++ GuapStalkerApi/GuapStalker.Domain/GuapContext.cs
@@ -0,0 +1,17 @@
+﻿using GuapStalker.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuapStalker.Domain;
+
+public class GuapContext: DbContext
+{
+    public GuapContext(DbContextOptions<GuapContext> opts): base(opts) {}
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<GuapReport> GuapReports { get; set; }
+    public DbSet<SavingsGoal> SavingsGoals { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Budget> Budgets { get; set; }
+    
+}
